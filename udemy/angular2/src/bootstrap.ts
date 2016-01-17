58b255eb611918774b7318e09c31fafed1f48154
tarun khemani <tkhemani@live.com>
2016-01-17 06:55:31 +0530

    change to udemy course for batarangle debugging in chrome

--- udemy/angular2/src/bootstrap.ts
+++ udemy/angular2/src/bootstrap.ts
@@ -5,4 +5,11 @@ import {ROUTER_PROVIDERS} from 'angular2/router';
 
 import {App} from './components/app';
 
-bootstrap(App, [ROUTER_PROVIDERS,HTTP_PROVIDERS]);
+//bootstrap(App, [ROUTER_PROVIDERS,HTTP_PROVIDERS]);
+
+
+import { AppViewListener } from 'angular2/src/core/linker/view_listener';
+import { DebugElementViewListener } from 'angular2/platform/common_dom';
+import { bind } from 'angular2/core';
+
+bootstrap(App, [bind(AppViewListener).toClass(DebugElementViewListener),ROUTER_PROVIDERS,HTTP_PROVIDERS]);
