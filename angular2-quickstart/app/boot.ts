ac380743a2713f5550a3361f6b6fe98babce77b2
tarun khemani <tkhemani@live.com>
2016-01-18 14:53:02 +0530

    routing

--- angular2-quickstart/app/boot.ts
+++ angular2-quickstart/app/boot.ts
@@ -4,8 +4,13 @@ import {AppComponent} from './components/app/app'
 import { AppViewListener } from 'angular2/src/core/linker/view_listener';
 import { DebugElementViewListener } from 'angular2/platform/common_dom';
 import { bind } from 'angular2/core';
-
+import {ROUTER_PROVIDERS,LocationStrategy, PathLocationStrategy} from 'angular2/router'
 //bootstrap(AppComponent);
+// var universalInjectables = [ 
+//   bind(LocationStrategy).toClass(PathLocationStrategy)
+// ];
 bootstrap(AppComponent, [
   bind(AppViewListener).toClass(DebugElementViewListener),
+  ROUTER_PROVIDERS
+//   universalInjectables, 
 ]);
--- angular2-quickstart/app/components/app/app.ts
+++ angular2-quickstart/app/components/app/app.ts
@@ -1,12 +1,19 @@
 import {Component} from 'angular2/core';
 import {NgIf, NgFor} from 'angular2/common';
 import {contactList} from '../contactList/contactList';
+import {NgIf, NgFor} from 'angular2/common';
+import {contactList} from '../contactList/contactList';
+import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router'
 @Component({
     selector: 'my-app',
-    directives: [contactList],
+    directives: [contactList, ROUTER_DIRECTIVES],
     templateUrl: 'app/components/app/app.html',
     styleUrls: ['app/components/app/app.css']
 })
+@RouteConfig([
+    {path:"/contacts", name: "Contacts", component: contactList, useAsDefault:true },
+    {path:"/newcontacts", name: "NewContacts", component: contactList }    
+])
 export class AppComponent { 
     
 }
