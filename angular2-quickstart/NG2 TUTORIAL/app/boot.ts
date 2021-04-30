caafa096d98e031a40b3f370d81900dfd92cde0a
Your Name <you@example.com>
2021-04-30 22:43:10 +0530

    m

--- angular2-quickstart/NG2 TUTORIAL/app/boot.ts
+++ angular2-quickstart/NG2 TUTORIAL/app/boot.ts
@@ -1,9 +1,16 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app'
+import {AppComponent} from './components/app/app'
+
 import { AppViewListener } from 'angular2/src/core/linker/view_listener';
 import { DebugElementViewListener } from 'angular2/platform/common_dom';
 import { bind } from 'angular2/core';
-
+import {ROUTER_PROVIDERS,LocationStrategy, PathLocationStrategy} from 'angular2/router'
+//bootstrap(AppComponent);
+// var universalInjectables = [ 
+//   bind(LocationStrategy).toClass(PathLocationStrategy)
+// ];
 bootstrap(AppComponent, [
   bind(AppViewListener).toClass(DebugElementViewListener),
+  ROUTER_PROVIDERS
+//   universalInjectables, 
 ]);
---
+++ angular2-quickstart/app/app.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {NgIf, NgFor} from 'angular2/common';
+@Component({
+    selector: 'my-app',
+    template: `
+             <h1>Hello World</h1>
+                `
+})
+export class AppComponent {
+
+}
